21ab800a95cbe6ebb8df368ed0acddfa23466b4e
just-sid <be10125.17@bitmesra.ac.in>
2020-10-11 00:02:44 +0530

    brought the list render function in the card list component using PROPS

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -3,5 +3,11 @@ import React from "react";
 import "./card-list.style.css";
 
 export const CardList = (props) => {
-  return <div className="card-list">{props.children}</div>;
+  return (
+    <div className="card-list">
+      {props.monsters.map((monster) => (
+        <h2 key={monster.id}> {monster.name} </h2>
+      ))}
+    </div>
+  );
 };
