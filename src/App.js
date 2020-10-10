0f835bd5f400411d08fef620ab01a5e6ee9da73d
just-sid <be10125.17@bitmesra.ac.in>
2020-10-10 16:26:53 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ monsters: users }));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hotel Transylvania</h1>
+        {this.state.monsters.map((monster) => (
+          <h2 key={monster.id}> {monster.name} </h2>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default App;
