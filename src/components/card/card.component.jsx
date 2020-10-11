2512a5a28a6787b26e5d9879214e464fc3e7422d
just-sid <be10125.17@bitmesra.ac.in>
2020-10-11 14:25:42 +0530

    added styling to the cards

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
+import './card.styles.css'
+
 export const Card = (props) => {
   return (
-    <div>
+    <div className='card-container'>
       <h2> {props.monster.name} </h2>
     </div>
   );
